a646204386660f00a3a39cd1b9aec12795319ed2
suwidhi <suwidhi@gmail.com>
2021-12-14 17:49:57 +0800

    tambah halaman login

---
+++ resources/views/register/register.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/line-awesome.min.css">
+    <title>Sign Up</title>
+</head>
+<body>
+    <div class="container">
+        <div class="p-bottom-3 p-top-3 trademark">
+            <img src="/img/trademark.png" alt="trademark logo">
+        </div>
+
+        <div class="m-bottom-3">
+            <h4 class="secondary-text p-bottom-1">Sign up to continue</h4>
+            
+            <button class="button button-primary button-large m-bottom-1">
+                Continue with email
+            </button>
+
+            <button class="button button-secondary button-large">
+                Use phone number
+            </button>
+        </div>
+
+        <div>
+            <p class="text-xs secondary-text">or sign up with</p>
+
+            <div class="flex-inline m-top-1 p-right-1">
+                <button class="button button-square m-left-1">
+                    <i class="la la-facebook-f"></i>
+                </button>
+
+                <button class="button button-square m-left-1">
+                    <i class="la la-google"></i>
+                </button>
+
+                <button class="button button-square m-left-1">
+                    <i class="la la-apple"></i>
+                </button>
+            </div>
+        </div>
+
+        <div class="m-top-3">
+            <a href="#" class="primary-text text-xs m-right-1">Terms of use</a>
+            <a href="#" class="primary-text text-xs ">Privacy Policy</a>
+        </div>
+    </div>
+</body>
+</html>
